5f482baa3f54efedc1f2712e4b851220e86a1530
mhalverstadt <markhalverstadt@gmail.com>
2022-05-09 12:23:55 -0600

    Today's Codewars

---
+++ Javascript/8kyu/Multiply.js
@@ -0,0 +1,14 @@
+// This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//   a * b
+// }
+
+// Mine: 
+function multiply(a, b){
+  return a * b
+}
+
+//Top Solution:
+multiply = function (a, b) {
+  return a * b;
+}

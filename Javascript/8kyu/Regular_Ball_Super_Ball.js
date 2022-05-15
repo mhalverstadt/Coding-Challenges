f3108fe7214f9df48cad06fc108dd8b2099ea802
mhalverstadt <markhalverstadt@gmail.com>
2022-05-15 12:13:44 -0600

    Today's Codewars

---
+++ Javascript/8kyu/Regular_Ball_Super_Ball.js
@@ -0,0 +1,19 @@
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+
+// Mine: 
+class Ball {
+  constructor ([ballType] = 'regular'){
+    this.ballType = ballType
+  }
+}
+
+
+//Top Solution:
+
+
+  
+//Test: 

af1f1df5480eedc709cd6f9d2b6a4bd7f8c0ee56
mhalverstadt <markhalverstadt@gmail.com>
2022-05-21 15:38:41 -0600

    Today's Codewars

---
+++ Javascript/8kyu/Drink_about.js
@@ -0,0 +1,53 @@
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+// Examples: (Input --> Output)
+
+// 13 --> "drink toddy"
+// 17 --> "drink coke"
+// 18 --> "drink beer"
+// 20 --> "drink beer"
+// 30 --> "drink whisky"
+
+//Mine:
+function peopleWithAgeDrink(old) {
+  switch (true){
+    case old < 14: return 'drink toddy'
+      break;
+    case old < 18 : return 'drink coke'
+      break;
+    case old < 21 : return 'drink beer'
+      break; 
+    case old >= 21 : return 'drink whisky'
+      break;
+  }
+};
+
+
+
+//Top Solution:
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
+
+//OR:
+var peopleWithAgeDrink = function(old) {
+  if (old>=21) return 'drink whisky';
+  if (old>=18) return 'drink beer';
+  if (old>=14) return 'drink coke';
+  return 'drink toddy';
+};
+
+
+//Test: 
+peopleWithAgeDrink(22)

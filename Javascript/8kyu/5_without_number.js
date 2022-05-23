60c852ee9190b637ce1e55f010cbb4262abc3a4d
mhalverstadt <markhalverstadt@gmail.com>
2022-05-23 16:40:07 -0600

    Today's Codewars

---
+++ Javascript/8kyu/5_without_number.js
@@ -0,0 +1,18 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+
+//Mine:
+function unusualFive() {
+  let gonnaBeFive = ['five', 'five', 'five', 'five', 'five']
+  return gonnaBeFive.length
+}
+
+//Top Solution:
+
+
+//Test: 
+unusualFive()

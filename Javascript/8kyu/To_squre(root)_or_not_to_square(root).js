0c4b48520ef323cd0ea82633f8532e23a33bdc6a
mhalverstadt <markhalverstadt@gmail.com>
2022-05-26 10:38:30 -0600

    Today's Codewars

---
+++ Javascript/8kyu/To_squre(root)_or_not_to_square(root).js
@@ -0,0 +1,28 @@
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+
+// Return a new array with processing every number of the input-array like this:
+
+// If the number has an integer square root, take this, otherwise square the number.
+
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
+
+//Mine:
+function squareOrSquareRoot(array) {
+  return array.map(num => Number.isInteger(Math.sqrt(num)) ? Math.sqrt(num) : num**2)
+}
+
+//Top Solution:
+function squareOrSquareRoot(array) {
+  return array.map(x => {
+    const r = Math.sqrt(x);
+    return (r % 1 == 0) ? r : (x*x);
+  });  
+}
+
+
+//Test: 
+var input = [ 4, 3, 9, 7, 2, 1 ];
+squareOrSquareRoot(input)

6e392dbe06d7e17253f753ac491733406eb011b2
mhalverstadt <markhalverstadt@gmail.com>
2022-06-03 10:59:07 -0600

    Another Codewars

---
+++ Javascript/8kyu/Grasshopper_Function_syntax_debugging.js
@@ -0,0 +1,17 @@
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+
+//Mine:
+function main (verb, noun){
+  return `${verb}${noun}`
+}
+
+
+//Top Solution:
+function main (verb, noun) {
+  return verb + noun
+}
+
+
+//Test:
+main('use ', 'sword')

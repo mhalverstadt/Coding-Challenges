d5a9c745a0d02f60218c6189d88d9256ad523e31
mhalaverstadt <markhalverstadt@gmail.com>
2022-06-04 14:49:45 -0600

    Another Codewars

---
+++ Javascript/8kyu/Hex_to_Decimal.js
@@ -0,0 +1,15 @@
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+
+//Mine:
+function hexToDec(hexString){
+  return parseInt(hexString, 16)
+}
+
+
+//Top Solution: SAME
+
+
+//Test:
+hexToDec("FF")

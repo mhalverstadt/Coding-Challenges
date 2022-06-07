850673eb48d33c62e64112666a6818ce4945d686
mhalverstadt <markhalverstadt@gmail.com>
2022-06-07 11:30:50 -0600

    Today's Codewars

---
+++ Javascript/6kyu/Counting_Duplicates.js
@@ -0,0 +1,30 @@
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+//Mine:
+function duplicateCount(text){
+  let arr = text.toUpperCase().split('')
+  console.log(arr)
+  let newarr = arr.filter((char, idx) => arr.indexOf(char) !== idx)
+  let result = ([...new Set(newarr)])
+  return result.length
+}
+
+//Top Solution:
+function duplicateCount(text){
+  return text.toLowerCase().split('').filter(function(val, i, arr){
+    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  }).length;
+}
+
+//Test:
+duplicateCount("aabBcde")
+duplicateCount("Indivisibility")

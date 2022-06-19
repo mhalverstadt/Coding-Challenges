cd285b1a99d8d6ea25cfb24478ea8b806b3f2646
mhalverstadt <markhalverstadt@gmail.com>
2022-06-19 15:57:49 -0600

    Today's Codewars

---
+++ Javascript/8kyu/Printing_Array_elements_with_comma_delimiters.js
@@ -0,0 +1,18 @@
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+//Mine:
+function printArray(array){
+  return array.join('')
+}
+
+//Top Solution:
+// Same
+
+//Test:
+printArray([2,3,4,5])

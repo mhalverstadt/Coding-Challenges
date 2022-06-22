b237feb6a89966be955180ccf9eb1e8b70522bd2
mhalverstadt <markhalverstadt@gmail.com>
2022-06-22 12:20:23 -0600

    Another Codewars

---
+++ Javascript/8kyu/Remove_first_and_ast_character_part_two.js
@@ -0,0 +1,32 @@
+// This is a spin off of my first kata.
+
+// You are given a string containing a sequence of character sequences separated by commas.
+
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+// Examples
+// "1,2,3"      =>  "2"
+// "1,2,3,4"    =>  "2 3"
+// "1,2,3,4,5"  =>  "2 3 4"
+
+// ""     =>  NULL
+// "1"    =>  NULL
+// "1,2"  =>  NULL
+
+
+//Mine:
+function array(arr){
+  let splitArr = arr.split(',')
+  return splitArr.length < 3 ? null : splitArr.slice(1, -1).join(' ')
+}
+
+//Top Solution:
+function array(arr){
+  return arr.split(",").slice(1,-1).join(" ") || null;
+}
+
+//Test:
+array('1,2,3,4,5')
+array('1,2')

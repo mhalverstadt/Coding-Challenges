35b55e959f77bb5fe8d1dc241a99205251730ae4
mhalverstadt <markhalverstadt@gmail.com>
2022-06-28 12:28:51 -0600

    Another Codewars

---
+++ Javascript/8kyu/Enumerable_Magic_3_Does_my_list_include_this.js
@@ -0,0 +1,16 @@
+//    
+
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+
+
+//Mine:
+function include(arr, item){
+  return arr.includes(item)
+}
+
+//Top Solution:
+function include(arr, item){
+  return arr.includes(item);
+}
+//Test:

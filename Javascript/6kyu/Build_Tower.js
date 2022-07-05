b47f0254848ace1c0e4b833870435622d4d0d7a5
mhalverstadt <markhalverstadt@gmail.com>
2022-07-05 16:08:57 -0600

    Another Codewars

--- Javascript/6kyu/Build_Tower.js
+++ Javascript/6kyu/Build_Tower.js
@@ -22,17 +22,29 @@
 // //Mine:
 function towerBuilder(nFloors) {
     let result =[]
+    let space = ' '
     let floor = '*'
-    for(let i = 0; result.length < nFloors; i++){
-        result.push(floor)
-        floor += '**'
+    for(let i = 0; i < nFloors; i++){
+        let length = (1+(nFloors-1)*2)
+        if (result.length < 1) {
+        result.push(`${floor.repeat(length)}`)
+        }else{
+          result.push(Array.from(`${space.repeat(i)}${floor.repeat(length - i)}`).fill(' ',-i).join(''))
+        }
     }
-    return result
+    return result.reverse()
   }
 
 // //Top Solution:
+function towerBuilder(n) {
+  return Array.from({length: n}, function(v, k) {
+    const spaces = ' '.repeat(n - k - 1);
+    return spaces + '*'.repeat(k + k + 1) + spaces;
+  });
+}
 
 // //Test:
+console.log (towerBuilder(4))
 console.log (towerBuilder(3))
 console.log (towerBuilder(2))
 console.log (towerBuilder(1))

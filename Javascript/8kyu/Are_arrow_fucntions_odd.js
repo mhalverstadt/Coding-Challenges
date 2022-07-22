1b3e8622e533b0252307b5b2967d7ce8dd5b9c3d
mhalverstadt <markhalverstadt@gmail.com>
2022-07-22 16:35:06 -0600

    Today's Codewars

---
+++ Javascript/8kyu/Are_arrow_fucntions_odd.js
@@ -0,0 +1,18 @@
+// Time to test your basic knowledge in functions! Return the odds from a list:
+
+// [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+// [2, 4, 6]        -->  []
+
+//Mine:
+const odds = values => values.filter(x => x % 2 !== 0)
+
+
+//Top Solution:
+function odds(values){
+  return values.filter( v => v%2 );
+}
+
+
+
+//Test: 
+console.log()

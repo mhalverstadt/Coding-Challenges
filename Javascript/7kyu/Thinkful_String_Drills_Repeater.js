ea02f213e15da4c6cfaa75f1a88fabe05a5ec712
mhalverstadt <markhalverstadt@gmail.com>
2022-07-23 10:39:45 -0600

    Another Codewars

---
+++ Javascript/7kyu/Thinkful_String_Drills_Repeater.js
@@ -0,0 +1,15 @@
+// Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+
+// Example: (Input1, Input2 --> Output)
+// "a", 5 --> "aaaaa"
+
+
+//Mine:
+const repeater = (string, n) => string.repeat(n)
+
+//Top Solution:
+
+
+
+//Test: 
+console.log()

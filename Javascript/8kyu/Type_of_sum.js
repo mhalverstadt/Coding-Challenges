8b468b5f747511709ce787762b105c68a93ea3e6
mhalverstadt <markhalverstadt@gmail.com>
2022-07-24 21:32:30 -0600

    Today's Codewars

---
+++ Javascript/8kyu/Type_of_sum.js
@@ -0,0 +1,14 @@
+// Return the type of the sum of the two arguments
+
+
+//Mine:
+const typeOfSum = (a,b) => typeof(a+b)
+
+
+//Top Solution:
+const typeOfSum = (a, b) => typeof(a + b);
+
+
+
+//Test: 
+console.log()

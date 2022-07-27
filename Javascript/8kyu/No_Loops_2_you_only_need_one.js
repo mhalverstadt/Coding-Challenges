b188b2dc13547b9fba923ce69c17c8b6aceb42a8
mhalverstadt <markhalverstadt@gmail.com>
2022-07-27 16:56:34 -0600

    Today's Codewars

---
+++ Javascript/8kyu/No_Loops_2_you_only_need_one.js
@@ -0,0 +1,16 @@
+// *** No Loops Allowed ***
+
+// You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+// Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+
+//Mine:
+const check = (a,x) => a.includes(x)
+
+//Top Solution:
+function check(a,b){
+    return a.includes(b);
+  };
+
+//Test:

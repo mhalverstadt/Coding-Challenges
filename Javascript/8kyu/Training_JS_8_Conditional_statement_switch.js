a98092d91fa37cdae71daa7ca366ab69c50dd32c
mhalverstadt <markhalverstadt@gmail.com>
2022-07-28 10:01:05 -0600

    Today's Codewars

---
+++ Javascript/8kyu/Training_JS_8_Conditional_statement_switch.js
@@ -0,0 +1,44 @@
+// Complete function howManydays, function accept 1 parameters:month, means the month of year, different month has different days (refer to the following table), return a number that how many days in this month(month is always greater than 0, less than or equal to 12).
+
+// +---------------+-------------+
+// |    month      |    days     |
+// +---------------+-------------+
+// |1,3,5,7,8,10,12|     31      |
+// +---------------+-------------+
+// |4,6,9,11       |     30      |
+// +---------------+-------------+
+// |2              |     28      |  (Do not consider the leap year)
+// +---------------+-------------+
+// little tips: Use default for most of the cases can reduce your work.
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+
+
+//Mine:
+function howManydays(month){
+  var days;
+  switch (month){
+    case 4: case 6: case 9: case 11: days = 30
+      break;
+    case 2 : days = 28
+      break;
+    default : days = 31
+      break
+  }
+  return days;
+}
+//Top Solution:
+function howManydays(month){
+  switch (month){
+     case 2: return 28
+     case 4:
+     case 6:
+     case 9:
+     case 11: return 30
+  }
+  return 31
+}
+
+//Test:

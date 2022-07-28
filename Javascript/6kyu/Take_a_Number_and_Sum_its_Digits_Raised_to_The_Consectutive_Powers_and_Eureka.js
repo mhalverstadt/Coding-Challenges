17c5c3f9dfef6a10c11093d8f4d3068646e872ac
mhalverstadt <markhalverstadt@gmail.com>
2022-07-28 11:40:18 -0600

    Today's Codewars

---
+++ Javascript/6kyu/Take_a_Number_and_Sum_its_Digits_Raised_to_The_Consectutive_Powers_and_Eureka.js
@@ -0,0 +1,47 @@
+// The number 89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number.
+
+// In effect: 89 = 8^1 + 9^2
+
+// The next number in having this property is 135.
+
+// See this property again: 135 = 1^1 + 3^2 + 5^3
+
+// We need a function to collect these numbers, that may receive two integers a, b that defines the range [a, b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+
+// Let's see some cases:
+
+// sumDigPow(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// sumDigPow(1, 100) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+// If there are no numbers of this kind in the range [a, b] the function should output an empty list.
+
+// sumDigPow(90, 100) == []
+// Enjoy it!!
+// .map(nested=>nested.map((val,i)=>val**(i+1)).reduce((a,b)=>a+b)))
+
+//Mine:
+function sumDigPow(a, b) {
+    let arr = Array.from({length: (b - a)+1}, (_, i) => a + i)
+    let nestedArr = arr.map(num => String(num).split('').map(x=>Number(x)))
+    const result = []
+    for(let i = 0; i < arr.length; i++){
+        if (nestedArr[i].map((val,index)=>val**(index+1)).reduce((a,b)=>a+b) == arr[i]){
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+//Top Solution:
+function sumDigPow(a, b) {
+    var ans = [];
+    while(a <= b){
+      if(a.toString().split('').reduce((x,y,i)=>x + +y ** (i + 1),0) == a)
+        ans.push(a);
+      a++;
+    }
+    return ans;
+  }
+
+//Test:
+console.log(sumDigPow(135,136))

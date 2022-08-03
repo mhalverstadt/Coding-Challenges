99113fc01c43fe0b3a9eb1ba3826f9078c112c84
mhalverstadt <markhalverstadt@gmail.com>
2022-08-03 11:46:06 -0600

    Today's Codewars

---
+++ Javascript/8kyu/Training_JS_6_Basic_data_types_boolean_and_conditional_statements_if_else.js
@@ -0,0 +1,15 @@
+// Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else, if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true".
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+
+//Mine:
+const trueOrFalse = val => val ? 'true' : 'false'
+
+//Top Solution:
+function trueOrFalse(val){
+  return Boolean(val).toString();
+}
+
+//Test:

632f5f03ce6333d24d6bcb9d53911358a021aa6a
mhalverstadt <markhalverstadt@gmail.com>
2022-08-16 19:57:59 -0600

    Today's Codewars

--- Javascript/6kyu/+1_Array.js
+++ Javascript/6kyu/+1_Array.js
@@ -56,21 +56,30 @@ function upArray(arr) {
     // if its 9 and more than one digit we have to check all previous digits
     // whether they are also a 9
     for (var j = arr.length - 1; j > -1; j--) {
-
         if (arr[j] !== 9) {
             arr[j] = arr[j] + 1;
             break;
         } else {
             arr[j] = 0;
         }
-
         if (j === 0) {
             arr.unshift(1);
         }
     }
-
     return arr;
 }
+
+//OR:
+//(imports bigint functionality in:)
+const BigNumber = require('bignumber.js');
+
+const upArray = arr =>
+  arr.length === 0 || arr.filter(x => x < 0 || x > 9).length > 0
+    ? null
+    : (new BigNumber(arr.join('')).plus(1))
+        .toPrecision()
+        .split('')
+        .map(Number);
 
 // Test:
 console.log(upArray([2,3,9]))
---
+++ Javascript/8kyu/get_ascii_value_of_character.js
@@ -0,0 +1,15 @@
+// Get ASCII value of a character.
+
+
+//Mine:
+const getASCII = c => c.charCodeAt(0)
+
+
+//Top Solution:
+const getASCII = c => c.charCodeAt(0);
+
+
+
+
+// Test:
+console.log()

b976f0e013baa7e1561308540361d7a014452969
mhalverstadt <markhalverstadt@gmail.com>
2022-09-09 18:05:08 -0600

    Another Codewars

--- Javascript/6kyu/Encrypt_this.js
+++ Javascript/6kyu/Encrypt_this.js
@@ -26,7 +26,12 @@ var encryptThis = function(text) {
 
 
 //Top Solution:
-
+const encryptThis = text => text
+  .split(' ')
+  .map(word => word
+  .replace(/(^\w)(\w)(\w*)(\w$)/, `$1$4$3$2`)
+  .replace(/^\w/, word.charCodeAt(0)))
+  .join(' ');
 
 //Test:
 console.log(encryptThis("Hello"))

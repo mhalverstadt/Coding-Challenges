16e06d413f4bc2e5d8f1b5e59414cecf40d0758b
mhalverstadt <markhalverstadt@gmail.com>
2022-09-30 12:22:44 -0600

    Today's Codewars

---
+++ Javascript/7kyu/Digits_explosion.js
@@ -0,0 +1,25 @@
+// Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+// Examples
+// explode("312")
+// should return :
+
+// "333122"
+// explode("102269")
+// should return :
+
+// "12222666666999999999"
+
+// Mine:
+function explode(s) {
+  return s.split('').map(num=>num.repeat(+num)).join('');
+}
+
+//Top Solution:
+function explode(s) {
+  return s.split("").map((e) => e.repeat(+e)).join("");
+}
+
+
+//Test:
+console.log(explode("102269"))

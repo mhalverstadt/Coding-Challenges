c5e70ea18ff7d7224dd2416fda62513064cb7d01
mhalverstadt <markhalverstadt@gmail.com>
2022-10-10 09:46:02 -0600

    Today's Codewars

---
+++ Javascript/7kyu/Nickname_Generator.js
@@ -0,0 +1,39 @@
+// Nickname Generator
+
+// Write a function, nicknameGenerator that takes a string name as an argument and returns the first 3 or 4 letters as a nickname.
+
+// If the 3rd letter is a consonant, return the first 3 letters.
+
+// nickname("Robert") //=> "Rob"
+// nickname("Kimberly") //=> "Kim"
+// nickname("Samantha") //=> "Sam"
+// If the 3rd letter is a vowel, return the first 4 letters.
+
+// nickname("Jeannie") //=> "Jean"
+// nickname("Douglas") //=> "Doug"
+// nickname("Gregory") //=> "Greg"
+// If the string is less than 4 characters, return "Error: Name too short".
+
+// Notes:
+
+// Vowels are "aeiou", so discount the letter "y".
+// Input will always be a string.
+// Input will always have the first letter capitalised and the rest lowercase (e.g. Sam).
+// The input can be modified
+
+//Mine:
+function nicknameGenerator(name){
+  let vowels = ['a','e','i','o','u']
+  if(name.length < 4){
+    return "Error: Name too short"
+  }
+  return vowels.includes(name[2]) ? name.substring(0,4) : name.substring(0,3)
+}
+
+//Top Solution:
+const nicknameGenerator = (name) => name.length > 3 
+  ? name.slice(0,  3 + 'aeiou'.includes(name[2])) 
+  : 'Error: Name too short'
+
+//Test:
+console.log(nicknameGenerator("Gregory"))

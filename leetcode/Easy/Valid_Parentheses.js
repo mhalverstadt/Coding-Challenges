11f9ab99c6a8ce7808f20daba400ec0d147cbdee
mhalverstadt <markhalverstadt@gmail.com>
2022-10-15 11:32:49 -0600

    working on Valid_Parentheses

---
+++ leetcode/Easy/Valid_Parentheses.js
@@ -0,0 +1,19 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+////////////////////////////////////////////////////////////////////////////////////
+
+//Will there only by bracket characters (no)? will there only be one set of any of the backets?
+
+//return boolean
+
+function validParentheses(){
+
+}
+
+console.log(validParentheses('(3ls)[3lkna{slakjdf]}'))

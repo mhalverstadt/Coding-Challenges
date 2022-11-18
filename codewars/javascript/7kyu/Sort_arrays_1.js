d86ba69211dabd87124cf303978f41f2e4774e82
mhalverstadt <markhalverstadt@gmail.com>
2022-11-18 09:57:31 -0700

    Daily Coding Challenge

---
+++ codewars/javascript/7kyu/Sort_arrays_1.js
@@ -0,0 +1,9 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+
+//Mine:
+sortme = function( names ){
+  return names.sort()
+}
+
+//Top Solution: SAME

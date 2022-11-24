245af26c856616eb0f5f4156074ca45ced7d1b93
mhalaverstadt <markhalverstadt@gmail.com>
2022-11-24 11:04:43 -0700

    Daily Coding Challenge

---
+++ codewars/javascript/8kyu/Enumerable_Magic_2_true_for_any.js
@@ -0,0 +1,16 @@
+// Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block (/function) returns true for any item in the array. If the array is empty, the function should return false.
+
+// Ruby: If you get stuck, you can read up here:
+
+// http://www.rubycuts.com/enum-any
+
+
+//Mine:
+function any(arr, fun){
+  return arr.some(fun)
+}
+
+//Top Solution:
+function any(arr, fun){
+  return arr.some(fun)
+}

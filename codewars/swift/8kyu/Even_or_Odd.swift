15f64a5d0e1ec58d8fdce32e4b9d6c5451913ea8
mhalverstadt <markhalverstadt@gmail.com>
2022-12-01 18:02:39 -0700

    Daily Coding Challenge

---
+++ codewars/swift/8kyu/Even_or_Odd.swift
@@ -0,0 +1,9 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+//Mine:
+func evenOrOdd(_ number:Int) -> String {
+    return number % 2 == 0 ? "Even" : "Odd"
+  }
+
+
+  

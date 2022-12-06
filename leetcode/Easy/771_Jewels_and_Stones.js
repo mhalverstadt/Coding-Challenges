9ec9a08eaf0beb8105a82a324a3904621fce2ecc
mhalverstadt <markhalverstadt@gmail.com>
2022-12-06 13:35:18 -0700

    Daily Code Challenge

--- leetcode/Easy/771_Jewels_and_Stones.js
+++ leetcode/Easy/771_Jewels_and_Stones.js
@@ -21,10 +21,9 @@
 
 //mine:
 function numJewelsInStones(jewels, stones){
-  let jewelsArr = jewels.split('')
   let counter = 0
   for(char of stones){
-    if(jewelsArr.includes(char)){
+    if(jewels.includes(char)){
       counter++
     }
   }
@@ -32,21 +31,21 @@ function numJewelsInStones(jewels, stones){
 }
 
 //solution with map:
-var numJewelsInStones = function(jewels, stones) {
-  const map = {}
- let count = 0
+// var numJewelsInStones = function(jewels, stones) {
+//   const map = {}
+//  let count = 0
 
- for (let char of jewels) {
-     map[char] = true
- }
+//  for (let char of jewels) {
+//      map[char] = true
+//  }
 
- for (let char of stones) {
-     if (map[char]) {
-         count++
-     }
- }
- return count;
-};
+//  for (let char of stones) {
+//      if (map[char]) {
+//          count++
+//      }
+//  }
+//  return count;
+// };
 
 
 

b6da21c40912bf612636831bbeb012943e3163eb
mhalverstadt <markhalverstadt@gmail.com>
2022-12-06 11:16:42 -0700

    Daily Coding Challenge

---
+++ leetcode/Easy/771_Jewels_and_Stones.js
@@ -0,0 +1,56 @@
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// Example 1:
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+
+// Example 2:
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+ 
+// Constraints:
+// 1 <= jewels.length, stones.length <= 50
+// jewels and stones consist of only English letters.
+// All the characters of jewels are unique.
+
+/////////////////////
+
+//input: two strings, case sensitive, 
+//return: number
+
+//mine:
+function numJewelsInStones(jewels, stones){
+  let jewelsArr = jewels.split('')
+  let counter = 0
+  for(char of stones){
+    if(jewelsArr.includes(char)){
+      counter++
+    }
+  }
+  return counter
+}
+
+//solution with map:
+var numJewelsInStones = function(jewels, stones) {
+  const map = {}
+ let count = 0
+
+ for (let char of jewels) {
+     map[char] = true
+ }
+
+ for (let char of stones) {
+     if (map[char]) {
+         count++
+     }
+ }
+ return count;
+};
+
+
+
+//test: 
+console.log(numJewelsInStones("aA", "aAAbbbb"), '3')
+console.log(numJewelsInStones("aAb", "aAcAcbbcbbc"), '7')
+console.log(numJewelsInStones("", ""), '0')

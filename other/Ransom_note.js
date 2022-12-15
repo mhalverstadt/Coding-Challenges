527c0a70cb4b1a99efb75dd2eaf7b4ff581e9b6e
mhalverstadt <markhalverstadt@gmail.com>
2022-12-15 15:34:58 -0700

    Daily Coding Challenge

---
+++ other/Ransom_note.js
@@ -0,0 +1,43 @@
+// Given a magazine of words and a ransom note, determine if it’s possible to “cut out” and create the ransom note from the magazine words.
+
+const magazine =
+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+
+
+function ransomNote(note){
+    let magMap = {}
+    for(word of magazine.split(' ')){
+        magMap[word] = magMap[word] +1 || 1
+    }
+    for(w of note.split(' ')){
+       if(!magMap[w]){
+        return false
+       }else{
+        magMap[w]--
+       }
+    }
+    return true
+}
+
+console.log(ransomNote("sit ad est sint"))
+console.log(ransomNote("sit ad est love"))
+
+
+describe("Ransom Note", () => {
+ it("Should return true", () => {
+  assert.equal(ransomNote("sit ad est sint", magazine), true);
+ });
+
+it("Should return false", () => {
+  assert.equal(ransomNote("sit ad est love", magazine), false);
+ });
+
+it("Should return true", () => {
+  assert.equal(ransomNote("sit ad est sint in in", magazine), true);
+ });
+
+it("Should return false", () => {
+  assert.equal(ransomNote("sit ad est sint in in in in", magazine), false);
+ });
+});
---
+++ other/Two_Sum_All_Pairs.js
@@ -0,0 +1,53 @@
+// Given an array of numbers, return all pairs that add up to a given sum. The numbers can be used more than once.
+
+////////
+//input: array and a target -> will ever be empty? always whole numbers?
+//output: array - format? duplicates?
+
+// function twoSum (array, target){
+//     let hashMap = {}
+//     let result = []
+//     for(const num of array){
+//         hashMap[num] = num
+//     }
+//     for(const num of array){
+//         if((target - num) in hashMap){
+//             result.push([num, hashMap[target - num]])
+//         }
+//     }
+//     return result 
+// }
+
+function twoSum(array, target){
+    let hashMap = {}
+    let result = []
+    for(const num of array){
+        let part2 = target - num
+        if(part2 in hashMap){
+            result.push([num, part2])
+        }
+        hashMap[num] = true
+    }
+    return result
+}
+
+
+//from https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038
+// const twoSum = (array, sum) => {
+//     const pairs = [];
+//     const store = [];
+  
+//     for (let part1 of array) {
+//       const part2 = sum - part1;
+//       if (store.indexOf(part2) !== -1) pairs.push([part1, part2]);
+//       store.push(part1);
+//     }
+  
+//     return pairs;
+//   };
+
+
+
+// test: 
+console.log(twoSum([1,2,2,3,4], 4))
+console.log(twoSum([1,2,2,3,4,5], 6))

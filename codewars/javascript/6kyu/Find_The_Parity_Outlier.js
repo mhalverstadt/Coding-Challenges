736be80f8e7c78b27f95d9ab2f1857ec710b75a7
mhalverstadt <markhalverstadt@gmail.com>
2022-12-29 11:57:04 -0700

    Daily Coding Challenge

---
+++ codewars/javascript/6kyu/Find_The_Parity_Outlier.js
@@ -0,0 +1,33 @@
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+
+///////////////////////////
+//input: array with only whole numbers, positive or negative, only one not odd or even
+//output: 
+
+
+//mine:
+function findOutlier(integers){
+    let evens = integers.filter(num => num % 2 === 0)
+    return evens.length === 1 ? evens[0] : integers.filter(num => num % 2 !==0)[0]
+}
+
+//top solution:
+function findOutlier(int){
+    var even = int.filter(a=>a%2==0);
+    var odd = int.filter(a=>a%2!==0);
+    return even.length==1? even[0] : odd[0];
+  }
+
+
+
+//test:
+console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
+console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]))

be427d6e105835b95026afea38a2c77080bb6abd
mhalverstadt <markhalverstadt@gmail.com>
2023-03-15 09:19:46 -0600

    Daily Coding Challege

---
+++ codewars/javascript/7kyu/Digitize.js
@@ -0,0 +1,20 @@
+// Given a non-negative integer, return an array / a list of the individual digits in order.
+
+// Examples:
+
+// 123 => [1,2,3]
+
+// 1 => [1]
+
+// 8675309 => [8,6,7,5,3,0,9]
+
+//mine:
+function digitize(n){
+  return String(n).split('').map(num => +num)
+}
+
+
+//top solution:
+function digitize(n) {
+  return String(n).split('').map(Number);
+}

af177ea3b0ad94a16ef1f4180dabd9f0735725a3
mhalverstadt <markhalverstadt@gmail.com>
2023-04-15 12:45:03 -0600

    Daily Coding Challege

---
+++ codewars/javascript/6kyu/Calculate_String_Rotation.js
@@ -0,0 +1,37 @@
+// Write a function that receives two strings and returns n, where n is equal to the number of characters we should shift the first string forward to match the second. The check should be case sensitive.
+
+// For instance, take the strings "fatigue" and "tiguefa". In this case, the first string has been rotated 5 characters forward to produce the second string, so 5 would be returned.
+
+// If the second string isn't a valid rotation of the first string, the method returns -1.
+// Examples:
+// "coffee", "eecoff" => 2
+// "eecoff", "coffee" => 4
+// "moose", "Moose" => -1
+// "isn't", "'tisn" => 2
+// "Esham", "Esham" => 0
+// "dog", "god" => -1
+
+function shiftedDiff(first,second){
+    if(first === second) return 0
+    let compareString = first
+    let counter = 0
+    for(letter of first){
+        let lastLetter = compareString.slice(-1)
+        let deleteLastLetter = compareString.slice(0, -1)
+        compareString = lastLetter + deleteLastLetter
+        counter++
+        if(compareString === second){
+            return counter
+        }
+    }
+    return -1
+}
+
+//top solution:
+function shiftedDiff(first, second) {
+    if (first.length != second.length) return -1
+    return (second + second).indexOf(first)
+  }
+
+//test:
+console.log(shiftedDiff("coffee", "eecoff"))

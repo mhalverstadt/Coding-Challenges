bb102e94bcd7043e5d22d8c2ddf27b4daf1f7f4d
mhalverstadt <markhalverstadt@gmail.com>
2023-05-02 09:55:59 -0600

    Daily Coding Challege

---
+++ codewars/javascript/6kyu/Permute_a_Palindrome.js
@@ -0,0 +1,25 @@
+// Write a function that will check whether ANY permutation of the characters of the input string is a palindrome. Bonus points for a solution that is efficient and/or that uses only built-in language functions. Deem yourself brilliant if you can come up with a version that does not use any function whatsoever.
+
+// Example
+// madam -> True
+// adamm -> True
+// junk -> False
+
+// Hint
+// The brute force approach would be to generate all the permutations of the string and check each one of them whether it is a palindrome. However, an optimized approach will not require this at all.
+
+
+function permuteAPalindrome (input) { 
+  let charMap = {}
+
+  for(const letter of input){
+    charMap[letter] = charMap[letter] + 1 || 1
+  }
+
+  return Object.values(charMap).filter(n => n % 2 !== 0).length <= 1
+}
+
+
+console.log(permuteAPalindrome('eeeaaeae'))
+console.log(permuteAPalindrome('eeeee'))
+console.log(permuteAPalindrome('eeaaeee'))

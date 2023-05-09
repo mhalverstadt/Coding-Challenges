5ec867090d0f00ac5fab2e84c503f7e3998a59b2
mhalverstadt <markhalverstadt@gmail.com>
2023-05-09 09:50:52 -0600

    Daily Coding Challege

---
+++ codewars/python/8kyu/Opposite_number.py
@@ -0,0 +1,18 @@
+"""
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+"""
+
+#mine:
+def opposite(number):
+    return number * -1
+
+
+#top solution:
+def opposite(number):
+    return -number

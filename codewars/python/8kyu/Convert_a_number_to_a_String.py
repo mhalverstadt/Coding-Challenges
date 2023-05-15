72a297672882d45564497f6746705a32e7a1d933
mhalverstadt <markhalverstadt@gmail.com>
2023-05-14 18:54:56 -0600

    Daily Coding Challege

--- codewars/python/8kyu/Convert_a_number_to_a_String.py
+++ codewars/python/8kyu/Convert_a_number_to_a_String.py
@@ -12,3 +12,21 @@ Examples (input --> output):
 #mine:
 def number_to_string(num):
     return str(num)
+
+#One of the top solutions:
+
+number_to_string = str
+
+"""
+explanation
+str is already a reserved keyword which is in-fact a function, therefore you can pass arguments to it. What is happening is it is making number_to_string equal to the str function. This means is the technically the same as the str function and any arguments that are passed to number_to_string are passed through the str function. There is no point having a function within a function (an embedded function), so you can access the function directly by simply stating it rather than wrapping it up in a lambda or a def function.
+
+It's the same as:
+
+def number_to_string(x):
+  return str(x)
+
+or 
+
+number_to_string = lambda x: str(x)
+"""
---
+++ codewars/python/8kyu/Convert_boolean_values_to_strings_yes_or_no.py
@@ -0,0 +1,7 @@
+"""
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+"""
+
+#mine:
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
